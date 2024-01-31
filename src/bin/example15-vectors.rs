882520954fa3b5713f93faa8d7ed1ad87f2f568b
lapeko <lapeko88@gmail.com>
2024-01-31 23:06:32 +0100

    use vector

---
+++ src/bin/example15-vectors.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let numbers = vec![10, 20, 30, 40];
+
+    for num in &numbers {
+        match num {
+            30 => println!("{}", "thirty"),
+            _ => println!("{}", num),
+        };
+    }
+
+    println!("\nLength is: {}", numbers.len());
+}
